8ef2f380162b560109ec8b0b1d803e8dfc2c9c1a
felipemslima <felipe.mslima07@gmail.com>
2024-12-07 00:36:51 -0300

    update sript

--- Registradores/exec.sh
+++ Registradores/exec.sh
@@ -1,11 +1,11 @@
-ghdl -a -fsynopsys mux.vhd
+ghdl -a -fsynopsys mux.vhd #Adicionar todos os arquivos que deseja compilar
 ghdl -a -fsynopsys reg.vhd
 ghdl -a -fsynopsys decodificador.vhd
 ghdl -a -fsynopsys regfile.vhd
 ghdl -a -fsynopsys regfile_tb.vhd
-ghdl -e -fsynopsys regfile_tb
-ghdl -r -fsynopsys regfile_tb --vcd=simul.vcd
+ghdl -e -fsynopsys regfile_tb #Entidade que voce quer rodar
+ghdl -r -fsynopsys regfile_tb --vcd=simul.vcd #Entidade que voce quer rodar e arquivo saida .vcd pra jogar no gtkwave
 # gtkwave simul.vcd
 # rm *.o 
-rm *.cf 
+rm *.cf #Arquivo gerado pelo ghdl se não quiser só comentar
 # rm *.lst
